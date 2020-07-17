316b448fc225a518104ea54234c5f2f30d8caa88
sabastianv <sabastian.vinodbella@springernature.com>
2020-07-17 23:23:50 +0200

    Use came case

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,14 +1,14 @@
 import bodyParser from "body-parser";
 import express, { Router } from "express";
-import * as JournalService from "./services/journal";
+import * as journalService from "./services/journal";
 
 const router: Router = express.Router();
 const jsonParser = bodyParser.json();
 
 router.route("/journal/:id*?")
-    .get(JournalService.get)
-    .post(jsonParser, JournalService.update)
-    .put(jsonParser, JournalService.create)
-    .delete(jsonParser, JournalService.remove);
+    .get(journalService.get)
+    .post(jsonParser, journalService.update)
+    .put(jsonParser, journalService.create)
+    .delete(jsonParser, journalService.remove);
 
 export = router;
