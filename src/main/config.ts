6635789226f08c26994e90943af729c81f79d4de
sabastianv <sabastian.vinodbella@springernature.com>
2020-07-18 02:49:26 +0200

    finalize config reader

--- src/main/config.ts
+++ src/main/config.ts
@@ -2,13 +2,18 @@
 import ts from "typescript";
 import config from "./application.json";
 
+const EXPRESSION_START = "{{";
+const EXPRESSION_END = "}}";
+
 const populateEnvironmentVariables = (obj: any): any => {
     Object.keys(obj)
         .forEach((key) => {
             const value = obj[key];
-            if (typeof value === "string") {
-                if (value.startsWith("process.env")) {
-                    obj[key] = eval(ts.transpile(value));
+            if (isString(value)) {
+                if (isExpression(value)) {
+                    const variableName = extractVariableName(value);
+                    const expression = constructExpression(variableName);
+                    obj[key] = evaluateExpression(expression) ?? value;
                 }
             } else {
                 obj[key] = populateEnvironmentVariables(value);
@@ -16,6 +21,21 @@ const populateEnvironmentVariables = (obj: any): any => {
         });
     return obj;
 };
+
+const isString = (value: any): boolean =>
+    typeof value === "string";
+
+const isExpression = (value: string): boolean =>
+    value.startsWith(EXPRESSION_START) && value.endsWith(EXPRESSION_END);
+
+const extractVariableName = (value: string): string =>
+    value.substr(EXPRESSION_START.length, value.length - (EXPRESSION_START.length + EXPRESSION_END.length)).trim();
+
+const constructExpression = (variableName: string): string =>
+    `process.env.${variableName}`;
+
+const evaluateExpression = (expression: string): string =>
+    eval(ts.transpile(expression));
 
 populateEnvironmentVariables(config);
 export = config;
